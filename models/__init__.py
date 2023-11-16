766c2dac8ea5e5fc965a41a369bc65de3a99eb40
uKaxg <uka.pgr@gmail.com>
2023-11-16 20:29:56 +0700

    change local resnet101

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,15 @@
 import torchvision
+import torch
 from .TDRG import TDRG
 
 model_dict = {'TDRG': TDRG}
 
 def get_model(num_classes, args):
-    res101 = torchvision.models.resnet101(pretrained=True)
+    try:
+        # res101 = torchvision.models.resnet101(weights='DEFAULT')
+        res101 = torchvision.models.resnet101()
+        res101.load_state_dict(torch.load('/kaggle/input/resnes101/resnet101-63fe2227.pth'))
+    except Exception as e:
+        print('Exception: {}'.format(e))
     model = model_dict[args.model_name](res101, num_classes)
     return model
